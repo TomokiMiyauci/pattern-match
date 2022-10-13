305e506c507ea589ce44df8d2abe92bff51fea6c
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2022-10-13 22:21:45 +0900

    chore(_tools): add npm script

---
+++ _tools/meta.ts
@@ -0,0 +1,35 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.31.0/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {},
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "pattern-matching",
+    version,
+    description: "Type-safe functional style pattern matching",
+    keywords: [
+      "pattern",
+      "match",
+      "pattern-match",
+      "pattern-matching",
+      "switch",
+      "fp",
+      "functional-programing",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/TomokiMiyauci/pattern-match",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/TomokiMiyauci/pattern-match.git",
+    },
+    bugs: {
+      url: "https://github.com/TomokiMiyauci/pattern-match/issues",
+    },
+    sideEffects: false,
+    type: "module",
+  },
+  packageManager: "pnpm",
+});
